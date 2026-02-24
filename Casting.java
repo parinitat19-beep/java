6ff07897f1848137e986c1215c75f1c8c631266e
parinitat19-beep <1nt23cb036.parinita@nmit.ac.in>
2026-02-24 23:04:43 +0530

    Day 1: DSA in JAVA

---
+++ Casting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Casting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = (int) (67.56f); // explicit casting from float to int 
+        System.out.println(num);
+        input.close();
+        }
+    }
+    
+
---
+++ integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class integer {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)) {
+        int num = input.nextInt();
+        System.out.println(num);
+        }
+        
+    }
+}
